a52a3daa7a99b082f453dc031af42040675d6fe8
Dan Carroll <dcarroll809@gmail.com>
2023-06-09 04:14:11 -0400

    Update hello.js

--- assets/js/hello.js
+++ assets/js/hello.js
@@ -3,41 +3,41 @@
 
 // script for use with the "hello" page
 
-const myImage = document.querySelector("img");
+let myImage = document.querySelector("img");
 
-myImage.onclick = () => {
-const mySrc = myImage.getAttribute("src");
-if (mySrc === "{{ site.baseurl }}/assets/images/GitHub_Logo.png") {
-    myImage.setAttribute("src", "{{ site.baseurl }}/assets/images/github-mark.png");
-    myImage.alt = "github mark";
-    myImage.style.width = "35%";
-} else {
-    myImage.setAttribute("src", "{{ site.baseurl }}/assets/images/GitHub_Logo.png");
-    myImage.alt = "github logo";
-    myImage.style.width = "50%";
-}
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "{{ site.baseurl }}/assets/images/GitHub_Logo.png") {
+        myImage.setAttribute("src", "{{ site.baseurl }}/assets/images/github-mark.png");
+        myImage.alt = "github mark";
+        myImage.style.width = "35%";
+    } else {
+        myImage.setAttribute("src", "{{ site.baseurl }}/assets/images/GitHub_Logo.png");
+        myImage.alt = "github logo";
+        myImage.style.width = "50%";
+    }
 }
 
 let myButton = document.querySelector("button");
 let myHeading = document.getElementById("whoIsIt");
 
 function setUserName() {
-const myName = prompt("Please enter your name.");
-if (!myName) {
+    let myName = prompt("Please enter your name.");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.innerHTML = myName;
+    }
+}
+
+if (!localStorage.getItem('name')) {
     setUserName();
 } else {
-    localStorage.setItem("name", myName);
-    myHeading.textContent = `${myName}`;
-}
-}
-
-if (!localStorage.getItem("name")) {
-setUserName();
-} else {
-const storedName = localStorage.getItem("name");
-myHeading.textContent = `${storedName}`;
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = storedName;
 }
 
-myButton.onclick = () => {
-setUserName();
+myButton.onclick = function() {
+    setUserName();
 }
